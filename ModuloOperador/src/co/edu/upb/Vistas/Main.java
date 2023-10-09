e6fdbe36a922b27203bdd1b78266c3996fb674c2
JCarvajalr <juandacr25@gmail.com>
2023-10-08 20:43:20 -0500

    ModuloOperador

---
+++ ModuloOperador/src/co/edu/upb/Vistas/Main.java
@@ -0,0 +1,18 @@
+package co.edu.upb.Vistas;
+
+import co.edu.upb.Vistas.Operador.Order;
+import co.edu.upb.Vistas.Operador.Product;
+import co.edu.upb.Vistas.Operador.VistaOperador;
+
+public class Main {
+
+     public static void main(String[] args) {
+        
+        //UserValidationApp logIn = new UserValidationApp();
+        //logIn.setVisible(true);
+        
+        VistaOperador ventana = new VistaOperador();
+        ventana.setVisible(true);
+        
+    }
+}   
---
+++ ModuloOperador/src/co/edu/upb/Vistas/Operador/Product.java
@@ -0,0 +1,23 @@
+package co.edu.upb.Vistas.Operador;
+
+public class Product {
+    int precio;
+    String nombre;
+    int tiempoCoccion; //0 -> Rapida, 1 -> Lenta
+    String id;
+    String descripcion;
+    
+    public Product(String nombre, String id, int precio, int tiempoCoccion, String descripcion){
+        this.nombre = nombre;
+        this.id = id;
+        this.precio = precio;
+        this.tiempoCoccion = tiempoCoccion;
+        this.descripcion = descripcion;
+    }
+    
+    @Override
+    public String toString(){
+        return nombre;
+    }
+    
+}
